ccdff78dcb694238ca9680ff7af1f30a0ca3fabb
shafiq <shafiqahmad081@gmail.com>
2024-08-09 19:53:55 +0600

    update header section and create hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html}"],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Frank: ["'Frank Ruhl Libre', serif;"],
+        Lato: ["Lato"],
+      },
+      colors: {
+        // Using modern `rgb`
+        primary: "rgb(var(--color-primary) / <alpha-value>)",
+        secondary: "rgb(var(--color-secondary) / <alpha-value>)",
+        secondary_white: "rgb(var(--color-white) / <alpha-value>)",
+        section_bg: "rgb(var(--color-section-bg) / <alpha-value>)",
+        footer_bg: "rgb(var(--color-footer-bg) / <alpha-value>)",
+        gray_bg: "rgb(var(--color-gray-bg) / <alpha-value>)",
+      },
+      spacing: {
+        "space-5.22": "22px",
+        "space-7.22": "30px",
+        "space-x-18": "75px",
+      },
+      fontSize: {
+        "3.2xl": "2rem", //32px
+      },
+      lineHeight: {
+        7.2: "1.875rem", //30px
+        11: "5.375rem", //86px
+      },
+    },
   },
   plugins: [],
 };
